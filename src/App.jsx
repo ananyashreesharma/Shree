4e6052264b94d77601b93bdd457adbea1463b6f3
anannyashreesharma <ananyashreesharma19@gmail.com>
2025-05-24 00:39:10 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import React from "react";
-
+import Hero from "./components/Hero.jsx";
 const App =()=>{
   return (
-    <main>
-<h1 classname="text=5xl font-bold font-circular-web"> hello</h1>
+    <main classname="relative min-h-screen w-screen overflow-x-hidden  ">
+
+<Hero/> 
       </main>
     
   );
